2b360599470b76b8d114ac0da40cc39af2ccd294
Maria <maria-d24@mail.ru>
2020-04-07 20:32:11 +0300

    Added greeting1

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,6 @@ import java.util.Arrays;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello, Masha!");
+        System.out.println("Hello, Github!");
     }
 }
